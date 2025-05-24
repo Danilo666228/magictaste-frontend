db4736cd99194bfa061015196681efaeddcf904f
Kovinskiy Danil <danyakovinskiy2000@mail.ru>
2025-05-24 20:03:43 +0300

    test category failed request

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -5,18 +5,18 @@ import { CLIENT_URL } from '@/lib/constants/url.constants'
 import { api } from '@/shared/api/instance'
 import { Category } from '@/shared/api/types'
 
-async function getCategories() {
-	try {
-		return await api.get<{ categories: Category[]; total: number }>('/category')
-	} catch (error) {
-		console.error('Failed to fetch categories:', error)
-		return { data: { categories: [], total: 0 } }
-	}
-}
+// async function getCategories() {
+// 	try {
+// 		return await api.get<{ categories: Category[]; total: number }>('/category')
+// 	} catch (error) {
+// 		console.error('Failed to fetch categories:', error)
+// 		return { data: { categories: [], total: 0 } }
+// 	}
+// }
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-	const categoriesResponse = await getCategories()
-	const categories = categoriesResponse.data.categories
+	// const categoriesResponse = await getCategories()
+	// const categories = categoriesResponse.data.categories
 
 	const routes: MetadataRoute.Sitemap = [
 		// {
